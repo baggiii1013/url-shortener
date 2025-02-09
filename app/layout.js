4842ea4b3a1a5585a6dbe1a2a85d960404904d9d
Kaustubh <138876995+baggiii1013@users.noreply.github.com>
2025-02-10 02:45:54 +0530

    initial commit

---
+++ app/layout.js
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Moktener - Your favourite url shortener",
+  description: "This app will allow you to shorten your urls with no effort ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar/>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '@/public/logo.svg'
+import { useMemo } from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className='bg-primary h-[70px] flex justify-around items-center'>
+          <div className="logo">  <Image
+      src={logo}
+      alt="Picture of the author"
+      className='w-[500px]'
+    /></div>
+            <ul className='flex gap-5 font-semibold font-mono text-2xl '>
+                <Link href="/"><li className='p-1'>Home</li></Link>
+                <Link href="/generate"><li className='p-1'>Shorten</li></Link>
+                <Link href="/about"><li className='p-1'>About Us</li></Link>
+                <Link href="/contact"><li className='p-1'>Contact Us</li></Link>
+                <li className='flex gap-4 items-center text-white'>
+                  <Link href="/generate"><button className='bg-btnColor py-1 px-3 rounded-lg'>Try now</button></Link>
+                  <Link href="/github"><button className='bg-btnColor py-1 px-3 rounded-lg'>GitHub</button></Link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
